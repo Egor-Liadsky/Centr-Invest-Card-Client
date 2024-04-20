fcac77f60a3719e9c171df953ceb304e56a4af7d
Egor-Liadsky <igeg91999@gmail.com>
2024-04-20 19:12:38 +0300

    added additional screens

--- feature/options/impl/src/main/java/com/turtleteam/impl/presentation/option/screen/OptionsScreen.kt
+++ feature/options/impl/src/main/java/com/turtleteam/impl/presentation/option/screen/OptionsScreen.kt
@@ -1,9 +1,15 @@
 package com.turtleteam.impl.presentation.option.screen
 
 import AdditionalView
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -34,6 +40,28 @@ fun OptionsScreen(
                 color = Color(0xFF2A2F33)
             ), modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 24.dp, bottom = 10.dp)
         )
+
+        Button(
+            onClick = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 20.dp, horizontal = 16.dp),
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF2A2F33)
+            )
+        ) {
+            Text(
+                text = "Привязать страховой полис",
+                style = TextStyle(
+                    fontFamily = FontFamily(Font(R.font.qanelas)),
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 14.sp,
+                    color = Color.White
+                ),
+                modifier = Modifier.padding(vertical = 10.dp)
+            )
+        }
 
         AdditionalView(viewModel)
     }
